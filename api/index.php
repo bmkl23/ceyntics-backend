e85519ad7f005059927759ad46a8a296a2f24a79
Kavinda <kbasnayake2000@gmail.com>
2026-03-12 00:59:34 +0530

    index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
+<?php
 
-<?php require __DIR__ . '/../public/index.php';
+// Handle CORS preflight
+header('Access-Control-Allow-Origin: https://ceyntics-frontend.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+require __DIR__ . '/../public/index.php';
