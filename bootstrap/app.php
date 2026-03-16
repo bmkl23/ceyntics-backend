68c3a6e2955be5b32617a23f996c952780283271
Kavinda <kbasnayake2000@gmail.com>
2026-03-16 15:08:16 +0530

    Remove withRouting conflict, fix RouteServiceProvider boot

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
