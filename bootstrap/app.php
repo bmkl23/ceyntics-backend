9353abffe685cb850a9cb501535e0e15ab098549
Kavinda <kbasnayake2000@gmail.com>
2026-03-16 16:34:12 +0530

    Add withRouting infrastructure + routes() wrapper

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
